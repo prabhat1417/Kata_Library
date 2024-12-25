f2fc3bcae07013271bfe0952996ebcfb7d8a28f0
prabhat1417 <prabhat14917@gmail.com>
2024-12-26 02:12:21 +0530

    added morgan to view request details

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 const libraryRoutes = require('./routes/libraryRoutes');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+logger.token('req-body', (req, res) => JSON.stringify(req.body));
+app.use(logger(':method :url :status - :response-time ms :req-body'));
+
 app.use('/api', libraryRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
